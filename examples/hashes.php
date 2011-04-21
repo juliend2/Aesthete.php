cf5cd188f32ac6e84419230f83723ac9d2315dcb
Julien Desrosiers <juliend2@gmail.com>
2011-04-20 20:21:17 -0400

    hash example

--- examples/hashes.php
+++ examples/hashes.php
@@ -7,6 +7,9 @@ h("
  another        = string
  number         = 23
  a_long_string  = this is a long string
+ a_multiline_string = 'Lorem ipsum dolor sit amet, 
+   consectetur adipiscing elit. In in mollis tortor. 
+   Curabitur libero quam, dapibus ultrices lacinia at, fringilla a ligula.'
 ")->each(function($k, $v){
   echo "{$k} => {$v}<br/>";
 });
